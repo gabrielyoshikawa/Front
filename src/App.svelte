df0dbcbb37ceb8d5f8fbaf17872a54aaf5682d22
Gabriel Yoshikawa Koti <gabrielkoti@hotmail.com>
2023-01-18 14:51:33 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Header from "./components/Header.svelte";
+
+  async function fetchData() {
+      const teste = await fetch("")
+    console.log(teste)
+      const data = await teste.json("http://localhost:5173/");
+    console.log(teste)
+
+    if (teste.ok) {
+      return data;
+    } else {
+      throw new Error(data);
+    }
+  }
+
+</script>
+
+<div class="container">
+  <Header/>
+
+</div>
+
+<style>
+
+</style>
