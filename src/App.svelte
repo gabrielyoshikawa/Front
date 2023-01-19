7437aa2cae40cf76b751580aa7a369eddc1d7d52
Gabriel Yoshikawa Koti <gabrielkoti@hotmail.com>
2023-01-19 14:25:07 -0300

    Commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,136 @@
 <script>
-  import Header from "./components/Header.svelte";
+  const testeFetch = async () => {
+    const testeResp = await fetch("http://localhost:3000/");
+    const cde = await testeResp.json();
 
-  async function fetchData() {
-      const teste = await fetch("")
-    console.log(teste)
-      const data = await teste.json("http://localhost:5173/");
-    console.log(teste)
+    console.log(cde);
+    return cde;
+  };
 
-    if (teste.ok) {
-      return data;
-    } else {
-      throw new Error(data);
-    }
-  }
-
+  const promise = testeFetch();
 </script>
 
 <div class="container">
-  <Header/>
+  <div class="titulo">
+    <img src="public/images/cloud.png" alt="" width="10%">
+    {#await promise then promise}
+    {#each Object.keys(promise) as key}
+      <!-- <p>{JSON.stringify(promise[key])}</p> -->
+    {/each}
+    <p>{JSON.stringify(promise.Friday.dayName)}</p>
+    {:catch error}
+      <p style="color: red">{error.message}</p>
+    {/await}
+  </div>
 
+  <div class="menu">
+    <button class="btn"><img src="public/images/menu-hamburguer.png" alt="" width="50%" height="50%"></button>
+  </div>
+
+  <div class="conteudo">
+      <div class="dia">
+
+        <div class="allTemps">
+          <p>Dia</p>
+          <div class="temperaturaMin">
+            {#await promise then promise}
+            <p>{JSON.stringify(promise.Friday.tempMin + " F")}</p>
+            {/await}
+          </div>
+
+          <div class="temperatura">
+            {#await promise then promise}
+            <p>{JSON.stringify(promise.Friday.temp + " F")}</p>
+            {/await}
+          </div>
+
+          <div class="temperaturaMax">
+            {#await promise then promise}
+            <p>{JSON.stringify(promise.Friday.tempMax + " F")}</p>
+            {/await}
+          </div>
+
+        </div>
+
+      </div>
+  </div>
 </div>
 
 <style>
+  * {
+      margin: 0;
+      padding: 0;
+    }
+    .titulo {
+      text-align: center;
+      color: rgb(70, 55, 55);
+      background-color: #ebf4f5;
+      height: 5%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-family: Arial, Helvetica, sans-serif;
+      font-weight: bold;
+      font-size: 100%;
+      padding: 2%;
+    }
+    .container {
+      background: white;
+      height: 95vh;
+      width: 100%;
+    }
+    .menu {
+      background: #6f84a5;
+      float: left;
+      height: 100%;
+      width: 15%;
+    }
+    .conteudo { 
+      background: #6f84a5;
+      float: left;
+      height: 100%;
+      width: 85%;
+    }
+    .dia {
+      font-family: Arial, Helvetica, sans-serif;
+      font-weight: bold;
+      font-size: 100%;
+      margin-top: 5%;
+      margin-left: 40%;
+    }
+    .allTemps {
+      text-align: center;
+      margin-left: -85%;
+      float: center;
+    }
+    .temperaturaMin {
+      font-family: Arial, Helvetica, sans-serif;
+      font-weight: bold;
+      font-size: 100%;
+      float: left;
+      margin-left: 30%;
+    }
+    .temperatura {
+      font-family: Arial, Helvetica, sans-serif;
+      font-weight: bold;
+      font-size: 100%;
+      float: left;
+    }
+    .temperaturaMax {
+      font-family: Arial, Helvetica, sans-serif;
+      font-weight: bold;
+      font-size: 100%;
+      float: left;
+    }
 
+    .btn {
+      width: 100%;
+      height: 8%;
+      background: #6f84a5;
+      border: none;
+      margin-top: 10%;
+    }
+    .btn:hover {
+      background-color: blue;
+    }
 </style>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,45 +1,11 @@
 <script>
-    function aaa() {
-        alert("Teste");
-    }
+
 </script>
 
-<div class="background">
-    <div class="titulo"></div>
-    <div class="menu">
-        <button onclick="aaa()">ABC</button>
-    </div>
-    <div class="conteudo"></div>
+<div>
+
 </div>
 
 <style>
-    * {
-        margin: 0;
-        padding: 0;
-        background-color: gray;
-    }
 
-    .background {
-        height: 100vh;
-        width: 100vw;
-    }
-
-    .titulo {
-        background: yellow;
-        height: 5%;
-        width: 100%;
-    }
-
-    .menu {
-        background: purple;
-        float: left;
-        height: 100%;
-        width: 13%;
-    }
-    .conteudo {
-        background: red;
-        float: left;
-        height: 100%;
-        width: 87%;
-    }
 </style>
