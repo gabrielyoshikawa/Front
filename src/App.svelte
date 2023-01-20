29e7fc88a5d4d68ed4aa682f18a4917fb83811de
Gabriel Yoshikawa Koti <gabrielkoti@hotmail.com>
2023-01-20 10:51:30 -0300

    Commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+    import Header from "./components/Header.svelte";
+
   const testeFetch = async () => {
     const testeResp = await fetch("http://localhost:3000/");
     const cde = await testeResp.json();
@@ -10,51 +12,136 @@
   const promise = testeFetch();
 </script>
 
-<div class="container">
-  <div class="titulo">
-    <img src="public/images/cloud.png" alt="" width="10%">
+<div class="titulo">
+    
+  <div class="titulo_conteudo">
+    <div class="setaEsquerda"><i class="fas fa-caret-left"></i></div>
+  </div>
+
+  <div class="titulo_conteudo">
+    <div class="display_dia">
+      {#await promise then promise}
+      {(promise.Today.dayName)}
+      {/await}
+    </div>
+  </div>
+
+  <div class="titulo_conteudo">
+    <div class="setaDireita"><i class="fas fa-caret-right"></i></div>
+  </div>
+
+</div>
+
+<div class="conteudo">
+
+  <div class="box_dia">
+
+    <div class="dia_noite">Dia</div>
+
+    <div class="conteudo_conteudo_temperatura">
+      {#await promise then promise}
+      {(promise.Today.tempMin + " F")}
+      {/await}
+    </div>
+
+    <div class="conteudo_conteudo_temperatura">
+      {#await promise then promise}
+      {(promise.Today.temp + " F")}
+      {/await}
+    </div>
+
+    <div class="conteudo_conteudo_temperatura">
+      {#await promise then promise}
+      {(promise.Today.tempMax + " F")}
+      {/await}
+    </div>
+
+    <div class="conteudo_icons">
+      <i class="fas fa-temperature-low"></i>
+    </div>
+
+    <div class="conteudo_icons">
+      <i class="fas fa-thermometer-half"></i>
+    </div>
+
+    <div class="conteudo_icons">
+      <i class="fas fa-temperature-high"></i>
+    </div>
+
+    <div class="conteudo_conteudo_vento">
+      {#await promise then promise}
+      {(promise.Today.windMin + " mph")}
+      {/await}
+    </div>
+
+    <div class="conteudo_conteudo_vento">
+      {#await promise then promise}
+      {(promise.Today.windAvg + " mph")}
+      {/await}
+    </div>
+
+    <div class="conteudo_conteudo_vento">
+      {#await promise then promise}
+      {(promise.Today.windMax + " mph")}
+      {/await}
+    </div>
+
+  </div>
+
+</div>
+
+<div class="box_noite">
+
+  <div class="dia_noite">Noite</div>
+
+  <div class="conteudo_conteudo_temperatura">
     {#await promise then promise}
-    {#each Object.keys(promise) as key}
-      <!-- <p>{JSON.stringify(promise[key])}</p> -->
-    {/each}
-    <p class="QQQQQ">{JSON.stringify(promise.Friday.dayName)}</p>
-    {:catch error}
-      <p style="color: red">{error.message}</p>
+    {(promise.Tonight.tempMin + " F")}
     {/await}
   </div>
 
-
-  <div class="conteudo">
-      <!-- <div class="dia">
-
-        <div class="allTemps">
-          <p>Dia</p>
-          <div class="temperaturaMin">
-            {#await promise then promise}
-            <p>{JSON.stringify(promise.Friday.tempMin + " F")}</p>
-            {/await}
-          </div>
-
-          <div class="temperatura">
-            {#await promise then promise}
-            <p>{JSON.stringify(promise.Friday.temp + " F")}</p>
-            {/await}
-          </div>
-
-          <div class="temperaturaMax">
-            {#await promise then promise}
-            <p>{JSON.stringify(promise.Friday.tempMax + " F")}</p>
-            {/await}
-          </div>
-
-        </div>
-
-      </div> -->
-      <div class="CCCCC"><h1>C</h1></div>
-      <div class="ABCDE"><h1>A</h1></div>
-      <div class="AAAAA"><h1>B</h1></div>
-      <div class="BBBBB"><h1>C</h1></div>
+  <div class="conteudo_conteudo_temperatura">
+    {#await promise then promise}
+    {(promise.Tonight.temp + " F")}
+    {/await}
   </div>
+
+  <div class="conteudo_conteudo_temperatura">
+    {#await promise then promise}
+    {(promise.Tonight.tempMax + " F")}
+    {/await}
+  </div>
+
+  <div class="conteudo_icons">
+    <i class="fas fa-temperature-low"></i>
+  </div>
+
+  <div class="conteudo_icons">
+    <i class="fas fa-thermometer-half"></i>
+  </div>
+
+  <div class="conteudo_icons">
+    <i class="fas fa-temperature-high"></i>
+  </div>
+
+  <div class="conteudo_conteudo_vento">
+    {#await promise then promise}
+    {(promise.Tonight.windMin + " mph")}
+    {/await}
+  </div>
+
+  <div class="conteudo_conteudo_vento">
+    {#await promise then promise}
+    {(promise.Tonight.windAvg + " mph")}
+    {/await}
+  </div>
+
+  <div class="conteudo_conteudo_vento">
+    {#await promise then promise}
+    {(promise.Tonight.windMax + " mph")}
+    {/await}
+  </div>
+
 </div>
 
 <style>
@@ -63,65 +150,92 @@
       padding: 0;
     }
     .titulo {
-      color: rgb(70, 55, 55);
-      background-color: #ebf4f5;
-      height: 5%;
-      font-family: Arial, Helvetica, sans-serif;
-      font-weight: bold;
-      font-size: 100%;
-      padding: 2%;
-    }
-    .QQQQQ {
-      text-align: center;
-    }
-    .container {
-      background: white;
-      height: 95vh;
+      background-image: linear-gradient(to right, #cdc9c9, #cfcccd, #d2d0d1, #d4d4d4, #d7d7d7);
       width: 100%;
+      height: 10%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .titulo_conteudo {
+      float: left;
+      padding: 20px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 20px;
+      padding-right: 5%;
+      padding-left: 5%;
+      font-family: Arial, Helvetica, sans-serif;
     }
     .conteudo { 
-      background: #6f84a5;
-      float: left;
-      height: 100%;
+      background-image: linear-gradient(to right, #cdc9c9, #cfcccd, #d2d0d1, #d4d4d4, #d7d7d7);      
+      height: 100vh;
       width: 100%;
+      position: relative;
     }
-    /* .dia {
-      font-family: Arial, Helvetica, sans-serif;
-      font-weight: bold;
-      font-size: 100%;
-      margin-top: 5%;
-      margin-left: 40%;
-    } */
-    .ABCDE {
-      background-color: red;
+
+    .box_dia {
+      background-image: linear-gradient(to right, #a7a2a2, #a19b9b, #9b9595, #958e8e, #8f8888);      
+      width: 300px;
+      height: 300px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin: -350px 0 0 -150px;
+      border-radius: 10px;
+    }
+
+    .box_noite {
+      background-image: linear-gradient(to right, #a7a2a2, #a19b9b, #9b9595, #958e8e, #8f8888);
+      width: 300px;
+      height: 300px;
+      position: absolute;
+      top: 103%;
+      left: 50%;
+      margin: -350px 0 0 -150px;
+      border-radius: 10px;
+    }
+
+    .conteudo_conteudo_temperatura {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 20px;
+      padding: 5%;
       float: left;
-      margin-left: 10%;
-      margin-right: 10%;
+      margin-left: 9%;
     }
-    .AAAAA {
-      background-color: blue;
+
+    .conteudo_conteudo_vento {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 20px;
       float: left;
-      margin-left: 10%;
-      margin-right: 10%;
+      background-color: white;
+      margin: 20px;
     }
-    .BBBBB {
-      background: green;
+
+    .conteudo_icons {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 20px;
+      padding: 5%;
       float: left;
+      margin-left: 5%;
       margin-left: 10%;
-      margin-right: 10%;
+      background-color: white;
     }
-    .CCCCC {
-      background-color: yellow;
-      text-align: center;
+    
+    .dia_noite {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 20px;
+      padding: 5%;
+      background-color: white;
     }
-    .btn {
-      width: 100%;
-      height: 8%;
-      background: #6f84a5;
-      border: none;
-      margin-top: 10%;
-    }
-    .btn:hover {
-      background-color: blue;
-    }
+
 </style>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,9 +1,9 @@
 <script>
-
+ export let text;
 </script>
 
 <div>
-
+    <button>{text}</button>
 </div>
 
 <style>
